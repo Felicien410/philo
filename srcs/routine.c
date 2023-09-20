353fdea20614c19c862d290031218e94a113b99b
Felicien410 <97636902+Felicien410@users.noreply.github.com>
2023-09-20 17:47:31 +0200

    emoji

--- srcs/routine.c
+++ srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: feliciencatteau <feliciencatteau@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 21:31:18 by fcatteau          #+#    #+#             */
-/*   Updated: 2023/09/20 17:30:26 by feliciencat      ###   ########.fr       */
+/*   Updated: 2023/09/20 17:47:23 by feliciencat      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	acquire_forks_and_eat(t_philo *philo)
 	if (philo->g->philo_died == 0 && philo->g->all_finish_philo == 0)
 	{
 		pthread_mutex_lock(&philo->g->enable_writing);
-		write_status("has taken a fork\n", philo);
-		write_status("has taken a fork\n", philo);
-		write_status("is eating\n", philo);
+		write_status("has taken a fork🍴\n", philo);
+		write_status("has taken a fork🍴\n", philo);
+		write_status("is eating🍜\n", philo);
 		pthread_mutex_unlock(&philo->g->enable_writing);
 		pthread_mutex_unlock(&philo->g->check_died);
 	}
@@ -43,7 +43,7 @@ void	print_sleep_routine(t_philo *philo)
 		if (!philo->g->philo_died)
 		{
 			pthread_mutex_lock(&philo->g->enable_writing);
-			write_status("is sleeping\n", philo);
+			write_status("is sleeping😴\n", philo);
 			pthread_mutex_unlock(&philo->g->enable_writing);
 
 			pthread_mutex_unlock(&philo->g->check_died);
@@ -70,7 +70,7 @@ void	print_thinking_routine(t_philo *philo)
 		if (!philo->g->philo_died)
 		{
 			pthread_mutex_lock(&philo->g->enable_writing);
-			write_status("is thinking\n", philo);
+			write_status("is thinking🤔\n", philo);
 			pthread_mutex_unlock(&philo->g->enable_writing);
 			pthread_mutex_unlock(&philo->g->check_died);
 			pthread_mutex_unlock(&philo->g->check);
